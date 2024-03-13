550b0f8d2f02d51c901b97283be2d00f29cfd325
Anzxwar <alif.katresna9@gmail.com>
2024-03-13 13:27:21 +0700

    Pratikum 2

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -1,25 +1,17 @@
 import { text } from "stream/consumers";
 
-export default function Tombol_1 () {
-    
+export function Tombol_2({isiPesan, namaTombol}) {
+    return (
+    <button
+        className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+        onClick={() => alert(isiPesan)}>
+        {namaTombol}
+    </button>
+    );
+}
+
+export default function Tombol_1 () {   
     function handleClick() {
         alert("Tombol telah ditekan!!!");
     }
-
-    function handleMouseOver() {
-        alert("Eits, mau mencet tombol ya?");
-    }
-
-    return (
-        <button 
-        className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
-        onClick={handleClick}
-        // onMouseOver={handleMouseOver}
-        onMouseLeave={() =>{
-            alert("Loh, kok sudah pergi!!!")}
-        }
-        >
-            Ini tombol
-        </button>
-    );
 }
